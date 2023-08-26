0157e69122c021f0fa82072988cda56ea88f166c
Nestor Fregoso <nestor.mio.fregoso@gmail.com>
2023-08-26 00:21:22 -0600

    Completed a basic proof of concept settings app. Got it to output the results to a json file to the users desktop.

--- updaterapp/src/index.js
+++ updaterapp/src/index.js
@@ -1,49 +1,40 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+function saveSettings() {
+    var settings = {};
+    settings.cTitle1 = document.getElementById('field-commentator1-name').value;
+    settings.cTitle2 = document.getElementById('field-commentator2-name').value;
+    settings.cTitle3 = document.getElementById('field-commentator3-name').value;
+    settings.game = document.getElementById('field-gametitle').value;
+    settings.mText1 = "";
+    settings.mText2 = "";
+    settings.mText3 = "";
+    settings.mText4 = "";
+    settings.p1Name = document.getElementById('field-player1-name').value;
+    settings.p1Score = document.getElementById('field-player1-score').value;
+    settings.p1Team = document.getElementById('field-player1-team').value;
+    settings.p2Name = document.getElementById('field-player2-name').value;
+    settings.p2Score = document.getElementById('field-player2-score').value;
+    settings.p2Team = document.getElementById('field-player2-team').value;
+    settings.round = document.getElementById('field-gameround').value;
+    settings.timestamp = Date.parse(document.getElementById('field-timestamp').value) / 1000;
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
+    window.electron.saveSettingsFile(JSON.stringify(settings));
+    document.getElementById('notification-saved').style.display = 'block';
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+function openTab(event, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("content-tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " is-active";
+}
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+function dismissMessage() {
+    document.getElementById('notification-saved').style.display = 'none';
+}
--- updaterapp/src/preload.js
+++ updaterapp/src/preload.js
@@ -1,2 +1,19 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { app, contextBridge } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const { homedir } = require('os');
+
+const getDesktopOrHomeDir = () => {
+    return path.resolve(homedir(), 'Desktop');
+  }
+
+contextBridge.exposeInMainWorld('electron', {
+    saveSettingsFile: (settings) => {
+        fs.writeFile(path.join(getDesktopOrHomeDir(), '/streamcontrol.json'),settings, function (err) {
+            if(err) throw err;
+            else console.log('Saved');
+        })
+    }
+});
